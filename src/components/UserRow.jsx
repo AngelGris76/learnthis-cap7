d35aa582323f1c16ff4d3e378769a9c215edf5f8
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2022-04-09 16:26:17 -0300

    capitulo 4.10

--- src/components/UserRow.jsx
+++ src/components/UserRow.jsx
@@ -1,28 +1,18 @@
+import UserStatus from './UserStatus';
+import UserRole from './UserRole';
 import style from './UserRow.module.css';
 
 const UserRow = ({ name, active, role }) => {
-  const activeClassName = active ? style.active : style.inactive;
-
-  const ROLE_STYLES = {
-    teacher: ['Profesor', style.teacher],
-    student: ['Alumno', style.student],
-    other: ['Otro', style.other],
-  };
-
-  const [roleName, roleClassName] = ROLE_STYLES[role] || ROLE_STYLES.other;
-
   return (
     <div className={style.userRow}>
       <div className={style.name}>
         <span>{name}</span>
       </div>
       <div className={style.status}>
-        <span className={activeClassName}>
-          {active ? 'Activo' : 'Inactivo'}
-        </span>
+        <UserStatus active={active} />
       </div>
       <div className={style.role}>
-        <span className={roleClassName}>{roleName}</span>
+        <UserRole role={role} />
       </div>
     </div>
   );

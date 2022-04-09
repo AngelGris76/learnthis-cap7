5f341301959071b7e340737847e01562e5dcefca
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2022-04-08 22:53:55 -0300

    capitulo 4.4

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,42 @@
 import ReactDOM from 'react-dom';
 // import App from './App';
 
-const Title = () => {
-  return <h1>Listado de usuarios</h1>;
+const USERS = [
+  { name: 'Pablo Castellanos', active: 'Activo', role: 'Profesor' },
+  { name: 'Javier López', active: 'Activo', role: 'Alumno' },
+  { name: 'Jose Miguel Fernandez', active: 'Activo', role: 'Profesor' },
+];
+
+const Title = ({ children }) => {
+  return <h1>{children}</h1>;
 };
 
-const User = (props) => {
+const User = ({ name, active, role }) => {
   return (
     <div className='user'>
-      <span className='name'>{props.name}</span>
-      <span className='active'>{props.active}</span>
-      <span className='role'>{props.role}</span>
+      <span className='name'>{name}</span>
+      <span className='active'>{active}</span>
+      <span className='role'>{role}</span>
+    </div>
+  );
+};
+
+const List = ({ users, children }) => {
+  const usersRendered = users.map((user) => <User key={user.name} {...user} />);
+
+  return (
+    <div className='list'>
+      {children}
+      {usersRendered}
     </div>
   );
 };
 
 const App = () => {
   return (
-    <div className='list'>
-      <Title />
-      <User name='Pablo Castellanos' active='Activo' role='Profesor' />
-      <User name='Jose Miguel Fernandez' active='Activo' role='Profesor' />
-    </div>
+    <List users={USERS}>
+      <Title>Listado de usuarios</Title>
+    </List>
   );
 };
 

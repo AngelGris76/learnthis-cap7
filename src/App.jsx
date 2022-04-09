c6424f098ad95f5c2883bab9845d735529c1c86c
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2022-04-09 13:12:15 -0300

    capitulo 4.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import UsesrsList from './components/UsersList';
+import Title from './components/Title';
+
+const USERS = [
+  { name: 'Pablo Castellanos', active: 'Activo', role: 'Profesor' },
+  { name: 'Javier López', active: 'Activo', role: 'Alumno' },
+  { name: 'Jose Miguel Fernandez', active: 'Activo', role: 'Profesor' },
+];
+
 const App = () => {
-  return null;
+  return (
+    <UsesrsList users={USERS}>
+      <Title>Listado de usuarios</Title>
+    </UsesrsList>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,4 @@
 import ReactDOM from 'react-dom';
-import UsesrsList from './components/UsersList';
-import Title from './components/Title';
-// import App from './App';
-
-const USERS = [
-  { name: 'Pablo Castellanos', active: 'Activo', role: 'Profesor' },
-  { name: 'Javier López', active: 'Activo', role: 'Alumno' },
-  { name: 'Jose Miguel Fernandez', active: 'Activo', role: 'Profesor' },
-];
-
-const App = () => {
-  return (
-    <UsesrsList users={USERS}>
-      <Title>Listado de usuarios</Title>
-    </UsesrsList>
-  );
-};
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));

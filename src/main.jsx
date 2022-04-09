7beed570b0e028bacb469d8ad241b2d61f3b2983
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2022-04-08 22:25:56 -0300

    capitulo 4.2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Title = () => {
+  return <h1>Listado de usuarios</h1>;
+};
+
+const User = (props) => {
+  return (
+    <div className='user'>
+      <span className='name'>{props.name}</span>
+      <span className='active'>{props.active}</span>
+      <span className='role'>{props.role}</span>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className='list'>
+      <Title />
+      <User name='Pablo Castellanos' active='Activo' role='Profesor' />
+      <User name='Jose Miguel Fernandez' active='Activo' role='Profesor' />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));

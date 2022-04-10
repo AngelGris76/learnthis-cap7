cc91cfce5a9c785cf02cf2173bce7435a20f5e8f
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2022-04-10 15:47:07 -0300

    capitulo 6.9, no controlado

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,14 +1,34 @@
+import { useState } from 'react';
 import UserRow from './UserRow';
 import style from './UsersList.module.css';
 
 const UsesrsList = ({ users, children }) => {
-  const usersRendered = users.map((user) => (
+  const [search, setSearch] = useState('');
+
+  const insensitiveSearch = search.toLowerCase();
+
+  const usersFiltered = search
+    ? users.filter((user) =>
+        user.name.toLowerCase().startsWith(insensitiveSearch)
+      )
+    : users;
+
+  const usersRendered = usersFiltered.map((user) => (
     <UserRow key={user.name} {...user} />
   ));
 
   return (
     <div className={style.usersList}>
       {children}
+      <form
+        onSubmit={(ev) => {
+          ev.preventDefault();
+          setSearch(ev.target.search.value);
+        }}
+      >
+        <input type='text' name='search' defaultValue='' />
+        <button type='submit'>Buscar</button>
+      </form>
       {usersRendered}
     </div>
   );

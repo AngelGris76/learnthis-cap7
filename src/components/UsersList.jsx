769184073482376ebeb24a4312042818f05561f0
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2022-04-10 18:28:29 -0300

    capitulo 7.2

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -5,7 +5,8 @@ import style from './UsersList.module.css';
 const UsesrsList = ({ users }) => {
   const [search, setSearch] = useState('');
 
-  const usersRendered = filterAndRenderUsers(users, search);
+  const usersFiltered = filterUsersByName(users, search);
+  const usersRendered = renderUsers(usersFiltered);
 
   return (
     <div className={style.usersList}>
@@ -24,15 +25,45 @@ const UsesrsList = ({ users }) => {
   );
 };
 
-const filterAndRenderUsers = (users, search) => {
-  const insensitiveSearch = search.toLowerCase();
-
+const filterUsersByName = (users, search) => {
+  /*
   const filteredUsers = search
     ? users.filter((user) =>
         user.name.toLowerCase().startsWith(insensitiveSearch)
       )
     : users;
-  return filteredUsers.map((user) => <UserRow key={user.name} {...user} />);
+
+  return filteredUsers;
+  */
+
+  if (!search) {
+    return users;
+  }
+
+  const insensitiveSearch = search.toLowerCase();
+
+  return users.filter((user) =>
+    user.name.toLowerCase().startsWith(insensitiveSearch)
+  );
+};
+
+const renderUsers = (users) => {
+  /*
+  const usersRendered =
+    users.length > 0 ? (
+      users.map((user) => <UserRow key={user.name} {...user} />)
+    ) : (
+      <p>No hay usuarios</p>
+    );
+
+  return usersRendered;
+  */
+
+  if (!users.length) {
+    return <p>No hay usuarios</p>;
+  }
+
+  return users.map((user) => <UserRow key={user.name} {...user} />);
 };
 
 export default UsesrsList;

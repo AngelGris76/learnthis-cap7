12ff36eae2cf46dfac96f50080cf528a251eafab
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2022-04-12 22:26:40 -0300

    capitulo 7.9

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -4,32 +4,23 @@ import style from './UsersList.module.css';
 import UsersListRows from './UsersListRows';
 
 const UsesrsList = ({ users }) => {
-  const [filters, setFilters] = useState({
-    search: '',
-    activeFilter: false,
-    order: 0,
-  });
+  const { search, activeFilter, order, setSearch, setActiveFilter, setOrder } =
+    useFilter();
 
-  let usersFiltered = filterByActive(users, filters.activeFilter);
-  usersFiltered = filterUsersByName(usersFiltered, filters.search);
-  usersFiltered = orderUsers(usersFiltered, filters.order);
+  let usersFiltered = filterByActive(users, activeFilter);
+  usersFiltered = filterUsersByName(usersFiltered, search);
+  usersFiltered = orderUsers(usersFiltered, order);
 
   return (
     <div className={style.usersList}>
       <h1>Listado de usuarios</h1>
       <UsersListFilters
-        search={filters.search}
-        setSearch={(search) => {
-          setFilters({ ...filters, search: search });
-        }}
-        activeFilter={filters.activeFilter}
-        setActiveFilter={(active) => {
-          setFilters({ ...filters, activeFilter: active });
-        }}
-        order={filters.order}
-        setOrder={(order) => {
-          setFilters({ ...filters, order: order });
-        }}
+        search={search}
+        setSearch={setSearch}
+        activeFilter={activeFilter}
+        setActiveFilter={setActiveFilter}
+        order={order}
+        setOrder={setOrder}
       />
 
       <UsersListRows users={usersFiltered} />
@@ -73,6 +64,26 @@ const orderUsers = (users, order) => {
     default:
       return copyUsers;
   }
+};
+
+const useFilter = () => {
+  const [filters, setFilters] = useState({
+    search: '',
+    activeFilter: false,
+    order: 0,
+  });
+
+  const setSearch = (search) => {
+    setFilters({ ...filters, search: search });
+  };
+  const setActiveFilter = (activeFilter) => {
+    setFilters({ ...filters, activeFilter: activeFilter });
+  };
+  const setOrder = (order) => {
+    setFilters({ ...filters, order: order });
+  };
+
+  return { ...filters, setSearch, setActiveFilter, setOrder };
 };
 
 export default UsesrsList;
